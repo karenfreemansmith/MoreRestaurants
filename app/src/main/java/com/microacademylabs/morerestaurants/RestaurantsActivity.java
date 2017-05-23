e2bf2a2ff22dede4041e0dee2bfd846237cc23a8
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-23 14:35:18 -0700

    add array adapter to display a list of restaurants

--- app/src/main/java/com/microacademylabs/morerestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/microacademylabs/morerestaurants/RestaurantsActivity.java
@@ -3,12 +3,22 @@ package com.microacademylabs.morerestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class RestaurantsActivity extends AppCompatActivity {
   private TextView locationText;
+  private ListView mRestaurantList;
+  private String[] restaurants = new String[] {
+      "Mi Mero Mole", "Mother's Bistro",
+      "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+      "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+      "Lardo", "Portland City Grill", "Fat Head's Brewery",
+      "Chipotle", "Subway"
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,10 @@ public class RestaurantsActivity extends AppCompatActivity {
     setContentView(R.layout.activity_restaurants);
 
     locationText = (TextView)findViewById(R.id.tvLocation);
+    mRestaurantList = (ListView)findViewById(R.id.restaurantListView);
+    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+    mRestaurantList.setAdapter(adapter);
+
     Intent intent = getIntent();
     String location = intent.getStringExtra("location");
     locationText.setText("Here are all the restaurants near " + location);
